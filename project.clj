5745c16308cb61872152f22b0c6df9f5b8a745a8
Aditya Rao <adityacrao@gmail.com>
2014-06-29 12:45:48 -0700

    Deployment changes

--- project.clj
+++ project.clj
@@ -3,4 +3,12 @@
   :url "https://github.com/acrao/jruby-sinatra-template"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm {:name "git"
+        :url "https://github.com/acrao/jruby-sinatra-template"}
+  :deploy-repositories [["clojars" {:creds :gpg}]]
+  :pom-addition [:developers [:developer
+                              [:name "Aditya Rao"]
+                              [:url "http://adityacrao.com"]
+                              [:email "username@users.noreply.github.com"]
+                              [:timezone "-8"]]]
   :eval-in-leiningen true)
